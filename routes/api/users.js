bc860e2bfd1a2faf8005ed13f3c61aefebf53705
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-13 19:56:33 +0100

    returning the JWT, checking with jwt.io

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -58,7 +60,23 @@ router.post(
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
-            res.send('User Registered');
+
+            // Return JsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 }, //time will be reduced at deploy
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server error');
