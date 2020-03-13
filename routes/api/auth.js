faad24af2cd2061da78d63f06f14476658290cb2
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-14 00:40:23 +0100

    added the token generation at the log in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 // @route       GET api/auth
@@ -16,5 +19,62 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server error.');
     }
 });
+
+// @route       POST api/auth
+// @desc        Authenticate user & get the token
+// @access      Public
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required.').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({
+                    errors: [{ msg: 'Invalid credentials' }]
+                });
+            }
+
+            // Match user passwords
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({
+                    errors: [{ msg: 'Invalid credentials' }]
+                });
+            }
+
+            // Return JsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 }, //time will be reduced at deploy
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 module.exports = router;
