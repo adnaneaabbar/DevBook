a8578a221577f877873ede0b239b077f05b796c9
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-16 21:50:59 +0100

    added the like and unlike posts routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route       GET api/profile/me
 // @desc        Get current user profile
