0345c06750fdb3a6d5691ada5c757d497cea4b5a
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-16 01:07:39 +0100

    added route for getting all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,7 +25,7 @@ router.get('/me', auth, async (req, res) => {
     }
 });
 
-// @route       GET api/profile
+// @route       POST api/profile
 // @desc        Create or Update user profile
 // @access      Private
 router.post(
@@ -107,5 +107,42 @@ router.post(
         }
     }
 );
+
+// @route       GET api/profile
+// @desc        Get all profiles
+// @access      Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', [
+            'name',
+            'avatar'
+        ]);
+        res.json(profiles);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server error.');
+    }
+});
+
+// @route       GET api/profile/user/:user_id
+// @desc        Get profile by user's id
+// @access      Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+        if (!profile) {
+            return res.status(400).json({ msg: 'Profile not found.' });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found.' });
+        }
+        res.status(500).send('Server error.');
+    }
+});
 
 module.exports = router;
