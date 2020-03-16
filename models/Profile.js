268abf1009718d06795d9d41373e932e000974d0
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-16 02:06:05 +0100

    adding and deleting education, similar to experience

--- models/Profile.js
+++ models/Profile.js
@@ -68,7 +68,7 @@ const ProfileSchema = new mongoose.Schema({
                 type: String,
                 required: true
             },
-            fielofstudy: {
+            fieldofstudy: {
                 type: String,
                 required: true
             },
