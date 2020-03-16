2082a8ee17e2a4af0ed7fdc34cd78c34323cda77
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-16 01:40:45 +0100

    new route for adding experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,7 +145,7 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
-// @route       GET api/profile
+// @route       DELETE api/profile
 // @desc        Delete profile, user & posts
 // @access      Private
 router.delete('/', auth, async (req, res) => {
@@ -161,5 +161,62 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server error.');
     }
 });
+
+// @route       PUT api/profile/experience
+// @desc        Add profile experience
+// @access      Private
+router.put(
+    '/experience',
+    [
+        auth,
+        [
+            check('title', 'Title is required.')
+                .not()
+                .isEmpty(),
+            check('company', 'Company name is required.')
+                .not()
+                .isEmpty(),
+            check('from', 'From date is required.')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            profile.experience.unshift(newExp);
+            await profile.save();
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error.');
+        }
+    }
+);
 
 module.exports = router;
