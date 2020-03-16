097978dbfe8dfcac0a6280af46f14c78e598466d
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-16 01:20:51 +0100

    added delete profile and user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,21 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route       GET api/profile
+// @desc        Delete profile, user & posts
+// @access      Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted.' });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server error.');
+    }
+});
+
 module.exports = router;
