a1470cfd2de03f966e1652cdf99674dac5cbdde7
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-17 18:28:16 +0100

    testing registration without using redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 export const Register = () => {
     // useState Hook
@@ -16,12 +17,30 @@ export const Register = () => {
     const onChange = e =>
         setFormData({ ...formData, [e.target.name]: e.target.value });
     // onChange handler
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password
+            };
+
+            try {
+                const config = {
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                };
+
+                const body = JSON.stringify(newUser);
+                const res = await axios.post('/api/users', body, config);
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     };
 
