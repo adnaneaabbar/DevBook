c2562be3ce11cc4dd1cf80a0db68d1938f3a432f
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-17 21:05:18 +0100

    creating a store for redux, basically boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,27 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path='/' component={Landing} />
-            <section className='container'>
-                <Switch>
-                    <Route exact path='/register' component={Register} />
-                    <Route exact path='/login' component={Login} />
-                </Switch>
-            </section>
-        </Fragment>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path='/' component={Landing} />
+                <section className='container'>
+                    <Switch>
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/login' component={Login} />
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
 );
 
 export default App;
