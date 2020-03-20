b6e543b7b9dc6234820232f2194ddd3e0b59c04a
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-20 15:43:20 +0100

    added the auth utils and the loadUser effect hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,25 +8,42 @@ import Register from './components/auth/Register';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Fragment>
-                <Navbar />
-                <Route exact path='/' component={Landing} />
-                <section className='container'>
-                    <Alert />
-                    <Switch>
-                        <Route exact path='/register' component={Register} />
-                        <Route exact path='/login' component={Login} />
-                    </Switch>
-                </section>
-            </Fragment>
-        </Router>
-    </Provider>
-);
+// we put this here too, because this will only check the first time that a user loads
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
+const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []); // empty brackets, so it becomes a componentDidMount and runs only one time
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Alert />
+                        <Switch>
+                            <Route
+                                exact
+                                path='/register'
+                                component={Register}
+                            />
+                            <Route exact path='/login' component={Login} />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
+};
 
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
+} from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -11,6 +16,13 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -19,6 +31,7 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 loading: false
             };
+        case AUTH_ERROR: // does the same thing as REGISTER_FAIL
         case REGISTER_FAIL:
             localStorage.removeItem('token');
             return {
