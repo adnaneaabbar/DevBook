5a2bd6e74dbc5e044109d60537c8d1f170a1ae4b
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-20 23:40:14 +0100

    added the getCurrentProfile linked to the profile/me route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,6 +22,7 @@ router.get('/me', auth, async (req, res) => {
                 .status(400)
                 .json({ msg: 'There is no profile for this user.' });
         }
+        res.json(profile);
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server error.');
