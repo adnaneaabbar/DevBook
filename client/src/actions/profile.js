03e8bdcc8a8e50d6bcd685a29fbc0e3be62fd19a
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-27 21:52:13 +0100

    added delete actions for education and experience

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -138,3 +138,51 @@ export const addEducation = (formData, history) => async dispatch => {
         });
     }
 };
+
+// Delete experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience deleted.', 'success'));
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Delete education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education deleted.', 'success'));
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
