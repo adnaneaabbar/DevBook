d54146fe35ac98eae0f3ff2492250ffd94e27fe0
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-03-27 23:54:41 +0100

    github repos action/reducer added

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert';
 import {
     GET_PROFILE,
     GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
@@ -44,6 +45,46 @@ export const getProfiles = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Get user profile by id
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Get Github Repositories
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         });
     } catch (err) {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,9 +1,10 @@
 import {
     GET_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
     PROFILE_ERROR,
     CLEAR_PROFILE,
-    UPDATE_PROFILE,
-    GET_PROFILES
+    UPDATE_PROFILE
 } from '../actions/types';
 
 const initialState = {
@@ -29,6 +30,12 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 profiles: payload,
+                loading: false
+            };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
                 loading: false
             };
         case PROFILE_ERROR:
